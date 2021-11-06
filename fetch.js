a688549b4e313fcb0922891ffec87d2011e3eed3
DraganSpasovski <74936833+DraganSpasovski@users.noreply.github.com>
2021-11-07 00:10:56 +0100

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -1,23 +1,14 @@
 let busEx;
 const getBtnBicikelj = document.getElementById('btnBicikeLJ');
-// trenutno se neuporabno
-//busRTExample()
 
-let busInfo = new L.circle([46.051318465073795, 14.479674887201202], {radius: 20, color: "black", fillOpacity: 0.8}).addTo(map);
-// TODO: implement avtobus info
+let busInfo = new L.circle([46.051318465073795, 14.479674887201202],
+     {radius: 20, color: "black", fillOpacity: 0.8}).addTo(map);
 busInfo.bindPopup("<b>registracija:</b> LJ-LPP-439" +
                "<br><b>pot:</b> KOLODVOR" + 
                "<br><b>Kapaciteta:</b> 80" +
                "<br><b>Zasedenost:</b> 32" +
                "<br><b>Hitrost:</b> 53");
-               /*
-               "<br><br><b>Lokacija:</b> ZOO" +
-               "<br><b>Naslednja postaja:</b> Večna pot" +
-               "<br><b>Prihod do naslednje postaje:</b> 2 minuti");*/
-// spreminjanje lokacije
-//busInfo.setLatLng([46.056129458185, 14.472137335197804]);
-
-
+               
 function sleep(ms)
 {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -35,25 +26,16 @@ function busRTExample()
 
                 for(let i = 0; i < data_len; i++)
                 {
-                    /* neuporabna verzija za "sleep" ker freeza program
-                    for(let j = 0; j < 1000; j++){
-                        if (j == -1) {
-                            break;
-                        }
-                        console.log(i);
-                    }*/
-                        //let test = new L.circle([data.results[0].trips[0].shape[i].lat, data.results[0].trips[0].shape[i].lng], {radius: 10}).addTo(map);
-
-                        // moralo bi premikati avtobus, vendar sleep ne dela
+                 
                         busEx._latlng.lat = data.results[0].trips[0].shape[i].lat;
                         busEx._latlng.lng = data.results[0].trips[0].shape[i].lng;
-                        //console.log(bus);
                 }
             } )
         .catch(e => console.log(e))
 }
 
 const getBtn = document.getElementById('getBtn');
+const getBtnAdd = document.getElementById('getBtnAdd');
 
 let schedule="<b>PRIHODI AVTOBUSOV</b><br><br>";
 var bus;
@@ -67,31 +49,41 @@ const getData = () => fetch("https://api.ontime.si/api/v1/lpp/stops/?page_size=1
 .then(response => response.json())
 .then(data =>
     {
-        layerGroup.clearLayers();
+    
+        layerGroup.clearLayers()
+        
         layerGroupBikes.clearLayers();
         for (result in data.results)
         { 
-            //var marker = new L.marker([data.results[result].lat,data.results[result].lng]).addTo(map)
             if (data.results[result].route_groups.includes(bus))
-            {//console.log(data.results[result]);
+            {   
                 let marker = new L.marker([data.results[result].lat,data.results[result].lng]).on('click', markerOnClick).addTo(layerGroup)
                 marker.bindTooltip(data.results[result].stop_id + "_" + data.results[result].name);
                 markerList.push(marker);
             }
         }
-        /*for(let i = 0; i < data.count;i++)
-        {
-            if (data.results[i].route_groups.includes("18") | data.results[i].route_groups.includes("14"))
-           {
-                var marker = new L.marker([data.results[i].lat,data.results[i].lng]).addTo(map)
-            }
-            
-            //marker.bindPopup("<b>Hello world!</b><br>I am {name}",{name:data.results[i].name}).openPopup();
-            ;
-        }*/
     } )
 .catch(e => console.log(e))
 
+const getDataAdd = () => fetch("https://api.ontime.si/api/v1/lpp/stops/?page_size=1367")
+.then(response => response.json())
+.then(data =>
+    {
+        for (result in data.results)
+        { 
+            if (data.results[result].route_groups.includes(bus))
+            {   
+                let marker = new L.marker([data.results[result].lat,data.results[result].lng]).on('click', markerOnClick).addTo(layerGroup)
+                marker.bindTooltip(data.results[result].stop_id + "_" + data.results[result].name);
+                markerList.push(marker);
+            }
+        }
+    } )
+.catch(e => console.log(e))
+
+function getRandomColor(x) {
+	return x.toString(16);
+}
 const getShape1 = () => fetch("https://api.ontime.si/api/v1/lpp/route-shapes/?groups="+bus)
 .then(response => response.json())
 .then(data =>
@@ -100,12 +92,9 @@ const getShape1 = () => fetch("https://api.ontime.si/api/v1/lpp/route-shapes/?gr
         let latlngs = [];
         for(let i = 0; i < data_len; i++)
         { 
-            //var marker = new L.marker([data.results[result].lat,data.results[result].lng]).addTo(map)
             latlngs.push([data.results[0].trips[0].shape[i].lat, data.results[0].trips[0].shape[i].lng]);
-            //data.results[0].trips[0].shape[i].lng
         }
-        //console.log(latlngs);
-        var polyline = L.polyline(latlngs, {color: 'red', opacity: 0.5}).addTo(layerGroup);
+        var polyline = L.polyline(latlngs, {color: "#" + ((1<<24)*Math.random() | 0).toString(16), opacity: 1,weight: 10, smoothFactor: 1}).addTo(layerGroup);
         map.fitBounds(polyline.getBounds());
     } )
 .catch(e => console.log(e))
@@ -118,12 +107,9 @@ const getShape2 = () => fetch("https://api.ontime.si/api/v1/lpp/route-shapes/?gr
         let latlngs = [];
         for(let i = 0; i < data_len; i++)
         { 
-            //var marker = new L.marker([data.results[result].lat,data.results[result].lng]).addTo(map)
             latlngs.push([data.results[0].trips[1].shape[i].lat, data.results[0].trips[1].shape[i].lng]);
-            //data.results[0].trips[0].shape[i].lng
         }
-        //console.log(latlngs);
-        var polyline = L.polyline(latlngs, {color: 'blue', opacity: 0.5}).addTo(layerGroup);
+        var polyline = L.polyline(latlngs, {color: "#" + ((1<<24)*Math.random() | 0).toString(16), opacity: 1, weight: 10, smoothFactor: 1}).addTo(layerGroup);
         map.fitBounds(polyline.getBounds());
     } )
 .catch(e => console.log(e))
@@ -173,7 +159,6 @@ const getBikes = () => fetch("https://api.ontime.si/api/v1/bicikelj/")
         for(bikeLoc in data.results)
         {
             
-            //console.log(data.results[bikeLoc].total_stands * 0.8)
             if(data.results[bikeLoc].total_stands * 0.8 <= data.results[bikeLoc].available_bikes)
             {
                 const c = L.circle([data.results[bikeLoc].lat,data.results[bikeLoc].lng],{radius: 20,color: 'green'}).bindPopup(
@@ -198,6 +183,11 @@ getBtn.addEventListener('click', cur_bus);
 getBtn.addEventListener('click', getData);
 getBtn.addEventListener('click', getShape1);
 getBtn.addEventListener('click', getShape2);
+getBtnAdd.addEventListener('click', cur_bus);
+getBtnAdd.addEventListener('click', getDataAdd);
+getBtnAdd.addEventListener('click', getShape1);
+getBtnAdd.addEventListener('click', getShape2);
+///getBtnAdd.addEventListener('click', addButton());
 getBtnBicikelj.addEventListener('click', getBikes);
 
 async function markerOnClick(e)
@@ -212,5 +202,4 @@ async function markerOnClick(e)
     }
     e.target.bindPopup(schedule);
     schedule = "<b>PRIHODI AVTOBUSOV</b><br><br>"
-    //console.log(stationID);
 }
